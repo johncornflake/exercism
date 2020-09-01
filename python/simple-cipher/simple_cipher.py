73e106aff9050f020cd7afb9284578151110c473
chad <chad@thegrommet.com>
2020-09-01 15:59:06 -0400

    solution done

--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -5,8 +5,7 @@ import secrets
 
 class Cipher:
     def __init__(self, key=None):
-        self.key = key
-        self.key_list = None
+        self.key = key or self.generateCipher()
 
     def encode(self, text):
         pairs = self.getPairs(text)
@@ -16,17 +15,9 @@ class Cipher:
         pairs = self.getPairs(text)
         return self.rotater(pairs, decode=True)
 
-    def getCipher(self, text_length):
+    def generateCipher(self):
         alphabet = self.generateAlphabet()
-        if self.key_list:
-            return self.key_list
-        elif self.key:
-            multiplier = math.ceil(text_length / len(self.key))
-            return [alphabet.index(i) for i in list(self.key)] * multiplier
-        else:
-            random_cipher = [secrets.choice(alphabet) for i in range(0,len)]
-            multiplier = math.ceil(text_length / len(random_cipher))
-            return random_ciper * multiplier
+        return ''.join([secrets.choice(alphabet) for i in range(0, 10)])
 
     def generateAlphabet(self):
         return deque([letter for letter in string.ascii_lowercase])
@@ -34,7 +25,6 @@ class Cipher:
     def rotater(self, pairs, decode=False):
         alphabet = self.generateAlphabet()
         res = ''
-        print(pairs)
         for pair in pairs:
             alphabet.rotate(alphabet.index(pair[0]) * -1)
             if decode:
@@ -45,11 +35,11 @@ class Cipher:
 
         return res
 
-    def getPairs(self, text):
-        key_list = self.getCipher(len(text))
-        return list(zip(list(text), key_list))
+    def cipherList(self, text_length):
+        alphabet = self.generateAlphabet()
+        multiplier = math.ceil(text_length / len(self.key))
+        return [alphabet.index(i) for i in list(self.key)] * multiplier
 
-'''
-TO DO:
-    - Need to auto-generate cipher. Keylist should probably not be an attribute since it's only used for rotater, just pass it around.
-'''
+    def getPairs(self, text):
+        key_list = self.cipherList(len(text))
+        return list(zip(list(text), key_list))
